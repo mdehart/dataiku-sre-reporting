eb00d02599f7e3c7400ac18c55d78347881f4c72
mikio.dehart@gmail.com <mikio.dehart@gmail.com>
2026-03-09 20:00:49 +0000

    Saved recipe: PAGERDUTYSREFETCHEVENTS.recipe_from_notebook_test_script_for_support_low_incidents_P2OE4T3

--- recipes/recipe_from_notebook_test_script_for_support_low_incidents_P2OE4T3.py
+++ recipes/recipe_from_notebook_test_script_for_support_low_incidents_P2OE4T3.py
@@ -1,17 +1,11 @@
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# test script for support_low_incidents P2OE4T3
 import dataiku
 import pandas as pd
 import requests
-from datetime import datetime
 
 # init
 API_KEY = "u+xqRscqCG91yXoHxL3g"
 POLICY_ID = "P2OE4T3"
-headers = {
-    "Accept": "application/vnd.pagerduty+json;version=2",
-    "Authorization": f"Token token={API_KEY}"
-}
+url = "https://api.pagerduty.com/incidents"
 
 # params
 params = {
@@ -21,28 +15,22 @@ params = {
     "sort_by": "created_at:desc"
 }
 
-# get call
-res = requests.get("https://api.pagerduty.com/incidents", headers=headers, params=params)
-incidents = res.json().get('incidents', [])
+# auth
+headers = {
+    "Accept": "application/vnd.pagerduty+json;version=2",
+    "Authorization": f"Token token={API_KEY}"
+}
 
-# table format conversion
-rows = []
-for inc in incidents:
-    rows.append({
-        "incident_number": inc.get('incident_number'),
-        "status": inc.get('status'),
-        "created_at": inc.get('created_at'),
-        "title": inc.get('title'),
-        "url": inc.get('html_url')
-    })
+res = requests.get(url, headers=headers, params=params).json()
 
-# write 
-df = pd.DataFrame(rows)
+# format into df
+df = pd.DataFrame([{
+    "incident_number": i.get('incident_number'),
+    "status": i.get('status'),
+    "created_at": i.get('created_at'),
+    "title": i.get('title'),
+    "url": i.get('html_url')
+} for i in res.get('incidents', [])])
 
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Recipe outputs
-# Note: Ensure "support_low_incidents_P2OE4T3" is exactly how the output appears on your Flow
-support_low_incidents_p2oe4t3 = dataiku.Dataset("support_low_incidents_P2OE4T3")
-
-# We pass 'df' here because that is the name of the DataFrame we created above
-support_low_incidents_p2oe4t3.write_with_schema(df)
+# output
+dataiku.Dataset("support_low_incidents_P2OE4T3").write_with_schema(df)
